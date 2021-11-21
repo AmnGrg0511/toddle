d55b26e9991895d01c8ba4ec2a2e9529a4713a69
AmnGrg05 <amanbtech05@gmail.com>
2021-11-21 17:24:42 +0530

    resolved drag and drop issues

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import {
 } from "react-icons/md";
 
 function App() {
+  const [active, setActive] = useState(-1);
+  const [base, setBase] = useState(-1);
   const [focus, setFocus] = useState(0);
   const [data, setData] = useState([{ title: "", depth: 0 }]);
   const lastRef = useRef(null);
@@ -26,10 +28,7 @@ function App() {
     console.log({ data, tree, depth });
     for (let i = 0; i < data.length; i++) {
       if (data[i].depth === depth) {
-        data[i].childs = treeify(
-          data.slice(i, part(data, i, depth)),
-          depth + 1
-        );
+        data[i].childs = treeify(data.slice(i, part(data, i)), depth + 1);
         tree.push(data[i]);
       }
     }
@@ -55,8 +54,10 @@ function App() {
     reader.readAsText(e.target.files[0]);
   };
 
-  const part = (d, i, depth) => {
-    let p = i + 1;
+  const part = (d, i) => {
+    if (i < 0) return;
+    let p = i + 1,
+      depth = d[i].depth;
     for (let j = i + 1; j < d.length; j++) {
       if (d[j].depth <= depth) break;
       p = j + 1;
@@ -66,6 +67,18 @@ function App() {
 
   return (
     <div
+      onDragEnd={() => {
+        if (active !== -1 && base !== -1)
+          setData((prev) => {
+            const p = part(data, active);
+            const temp = prev.slice(active, p);
+            prev = prev.filter((el, j) => j < active || j >= p);
+            let newBase = active <= base ? base + 1 - p + active : base + 1;
+            return [...prev.slice(0, newBase), ...temp, ...prev.slice(newBase)];
+          });
+        setBase(-1);
+        setActive(-1);
+      }}
       style={{
         width: "80%",
         margin: "50px auto",
@@ -73,7 +86,7 @@ function App() {
     >
       <div
         style={{
-          borderBottom: "2px solid #eee",
+          borderBottom: "4px solid #eee",
           display: "flex",
         }}
       >
@@ -91,93 +104,110 @@ function App() {
         />
       </div>
       {data.map(({ title, depth }, i) => (
-        <div
-          draggable
-          onDragStart={() => {
-            console.log("hi");
-          }}
-          key={i}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            borderBottom: "2px solid #eee",
-          }}
-        >
-          <div>
-            <IconButton onClick={() => {}}>
-              <RiDragMove2Fill />
-            </IconButton>
-            <IconButton
-              onClick={() => {
-                if (i !== 0 && depth > 0) {
+        <div>
+          <div
+            draggable
+            onDragStart={() => {
+              setActive(i);
+            }}
+            key={i}
+            style={{
+              display: "flex",
+              opacity: i >= active && i < part(data, active) ? 0.2 : 1,
+              alignItems: "center",
+            }}
+          >
+            <div>
+              <IconButton onClick={() => {}}>
+                <RiDragMove2Fill />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  if (i !== 0 && depth > 0) {
+                    setData((prev) =>
+                      prev.map((e, j) =>
+                        j >= i && j < part(prev, i)
+                          ? { ...e, depth: e.depth - 1 }
+                          : e
+                      )
+                    );
+                  }
+                }}
+              >
+                <RiArrowLeftLine />
+              </IconButton>
+              <IconButton
+                onClick={() => {
+                  if (i !== 0 && depth <= data[i - 1].depth) {
+                    setData((prev) =>
+                      prev.map((el, j) =>
+                        i === j ? { ...el, depth: el.depth + 1 } : el
+                      )
+                    );
+                  }
+                }}
+              >
+                <RiArrowRightLine />
+              </IconButton>
+              <IconButton
+                onClick={() =>
                   setData((prev) =>
-                    prev.map((e, j) =>
-                      j >= i && j < part(prev, i, depth)
-                        ? { ...e, depth: e.depth - 1 }
-                        : e
-                    )
-                  );
+                    prev.filter((e, j) => j < i || j >= part(prev, i))
+                  )
                 }
+              >
+                <RiDeleteBinLine />
+              </IconButton>
+            </div>
+            <div
+              style={{
+                height: 56,
+                width: 40,
+                backgroundColor: "#fafafa",
+                marginLeft: 15 + depth * 24,
+                marginRight: 15,
               }}
-            >
-              <RiArrowLeftLine />
-            </IconButton>
-            <IconButton
-              onClick={() => {
-                if (i !== 0 && depth <= data[i - 1].depth) {
-                  setData((prev) =>
-                    prev.map((el, j) =>
-                      i === j ? { ...el, depth: el.depth + 1 } : el
-                    )
-                  );
+            />
+            <textarea
+              rows="1"
+              ref={i === focus ? lastRef : null}
+              value={title}
+              style={{
+                opacity: 1 - depth * 0.1,
+                fontSize: 26 - depth * 2.5,
+              }}
+              onChange={({ target: { value } }) => {
+                const lines = value.split("\n");
+                value = lines[0];
+                let newData = data.map((el, j) =>
+                  i === j ? { ...el, title: value } : el
+                );
+                if (lines.length > 1) {
+                  const newNode = { title: lines[1], depth: data[i].depth };
+                  newData = [
+                    ...newData.slice(0, i + 1),
+                    newNode,
+                    ...newData.slice(i + 1),
+                  ];
+                  setFocus(i + 1);
                 }
+                setData(newData);
               }}
-            >
-              <RiArrowRightLine />
-            </IconButton>
-            <IconButton
-              onClick={() =>
-                setData((prev) =>
-                  prev.filter((e, j) => j < i || j >= part(prev, i, depth))
-                )
-              }
-            >
-              <RiDeleteBinLine />
-            </IconButton>
+            />
           </div>
           <div
-            style={{
-              height: 56,
-              width: 40,
-              backgroundColor: "#fafafa",
-              marginLeft: 15 + depth * 18,
-              marginRight: 15,
+            onDragOver={() => {
+              if (
+                data[active].depth - 1 <= depth &&
+                (!data[i + 1] || data[i + 1].depth <= data[active].depth)
+              )
+                setBase(i);
             }}
-          />
-          <textarea
-            rows="1"
-            ref={i === focus ? lastRef : null}
-            value={title}
             style={{
-              opacity: 1 - depth * 0.1,
-              fontSize: 26 - depth * 2.5,
-            }}
-            onChange={({ target: { value } }) => {
-              const lines = value.split("\n");
-              value = lines[0];
-              let newData = data.map((el, j) =>
-                i === j ? { ...el, title: value } : el
-              );
-              if (lines.length > 1) {
-                const newNode = { title: lines[1], depth: data[i].depth };
-                newData = [
-                  ...newData.slice(0, i + 1),
-                  newNode,
-                  ...newData.slice(i + 1),
-                ];
-                setFocus(i + 1);
-              }
-              setData(newData);
+              width: "100%",
+              height: base === i ? 40 : 4,
+              backgroundColor: base === i ? "#bde0f5" : "#eee",
+              transition: "all ease .2s",
             }}
           />
         </div>
@@ -189,7 +219,7 @@ function App() {
           onClick={() => {
             setData((prev) => [
               ...prev,
-              { title: "", depth: prev[prev.length - 1].depth ?? 0 },
+              { title: "", depth: prev[prev.length - 1]?.depth ?? 0 },
             ]);
           }}
         >
